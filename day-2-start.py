6cc7cb60969613e294446c0fc17e3c785f3edf6b
nivedt <92209836+nivedt@users.noreply.github.com>
2021-11-14 10:26:09 +0530

    Create day-2-start.py

---
+++ day-2-start.py
@@ -0,0 +1,36 @@
+#Data Types
+
+#string
+
+# print("Hello"[0])
+
+# print("123" + "456")
+# integer
+
+# print(123 + 345)
+# num_char=len(input("What is your name?"))
+# print("Your nme has" + num_char + "charcters.")
+
+# num_char = len(input("What is your name?"))
+# new_num_char=str(num_char)
+# print("Your name is" + new_num_char + "characters.")
+
+# a=float(123)
+# print(type(a))
+
+# print(70 + float(100.5))
+
+# print(str(70) + str(100))
+
+# print(3*3+3/3-3)
+
+# print(round(8/3,2))
+# score = 0
+# print("Your score is "+ str(score))
+
+
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"your score is {score}, your height is {height}, you are Winning is {isWinning}")
