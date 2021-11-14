e80145a90eedabd57d465a33b04bc90d907e86ea
nivedt <92209836+nivedt@users.noreply.github.com>
2021-11-14 10:20:25 +0530

    Create day-1-3-exercise.py

---
+++ day-1-3-exercise.py
@@ -0,0 +1,15 @@
+print( len( input("your name?") ) )
+name = input("What is your name?")
+print(name)
+
+
+name="jack"
+print(name)
+
+
+name=input("What is your name?")
+length=len(name)
+print(length)
+
+nama="jack"
+print(nama)
